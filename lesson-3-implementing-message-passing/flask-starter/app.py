df4adae47381b3768a6cfcfd08adaa24a8305514
Eduard Schildner <eduard.schildner@gmail.com>
2024-01-09 18:22:05 +0100

    Implement comuters endpoint.

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -10,5 +10,17 @@ app = Flask(__name__)
 def health():
     return jsonify({'response': 'Hello World!'})
 
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computers():
+    if request.method == 'GET':
+        return Response(json.dumps(retrieve_orders()),
+                        200,
+                        {'Content-Type': 'application/json'})
+    elif request.method == 'POST':
+        request_body = request.json
+        return Response(json.dumps(create_order(request_body)))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
 if __name__ == '__main__':
     app.run()
